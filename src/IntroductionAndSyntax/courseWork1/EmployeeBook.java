aabd471531229e8c2a72fa5b3d608045c1a4059a
GURitch <guritch1@mail.ru>
2023-02-09 00:30:17 +0300

    courseWorkInProcess

--- src/IntroductionAndSyntax/courseWork1/EmployeeBook.java
+++ src/IntroductionAndSyntax/courseWork1/EmployeeBook.java
@@ -1,4 +1,39 @@
 package IntroductionAndSyntax.courseWork1;
 
 public class EmployeeBook {
+    private final Employee[] employees;
+    private int count;
+
+    public EmployeeBook(int size) {
+        this.employees = new Employee[size];
+    }
+
+    public void addEmployee(String fullName, int department, int salary){
+        if(count == employees.length){
+            System.out.println("Нельзя добавить сотрудника, штат полон");
+        } else {
+            Employee employee = new Employee(fullName, department, salary);
+            employees[count] = employee;
+            System.out.println("ДОБАВЛЕН "+employees[count].toString());
+            count++;
+        }
+    }
+
+    public void deleteEmployeeById(int id) {
+        for (int i = 0; i < count; i++) {
+            if (employees[i].getId() == id) {
+                System.out.println("УДАЛЕН "+employees[i].toString());
+                System.arraycopy(employees, i + 1, employees, i, count - i - 1);
+                employees[count - 1] = null;
+                count--;
+                return;
+            }
+        }
+    }
+
+    public void printAllEmployees() {
+        for (int i = 0; i < count; i++) {
+            System.out.println(employees[i].toString());
+        }
+    }
 }
--- src/IntroductionAndSyntax/courseWork1/Main.java
+++ src/IntroductionAndSyntax/courseWork1/Main.java
@@ -2,6 +2,27 @@ package IntroductionAndSyntax.courseWork1;
 
 public class Main {
     public static void main(String[] args) {
+        EmployeeBook employeeBook = new EmployeeBook(5);
+
+        employeeBook.addEmployee("Иванов",1,30_000);
+        employeeBook.addEmployee("Артемов",1,30_000);
+        employeeBook.addEmployee("Борисов",1,30_000);
+        employeeBook.addEmployee("Иванов",1,30_000);
+        employeeBook.addEmployee("Иванов",1,30_000);
+        System.out.println();
+
+        employeeBook.printAllEmployees();
+        System.out.println();
+
+        employeeBook.deleteEmployeeById(3);
+        System.out.println();
+
+        employeeBook.printAllEmployees();
+        System.out.println();
+
+        employeeBook.addEmployee("Егоров",1,30_000);
+        System.out.println();
+        employeeBook.printAllEmployees();
 
     }
 }
