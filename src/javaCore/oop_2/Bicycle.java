9248356b7fdc27fc3f245ba2f08d9e8df69947c5
GURitch <guritch1@mail.ru>
2023-03-09 08:19:20 +0300

    initial commit

---
+++ src/javaCore/oop_2/Bicycle.java
@@ -0,0 +1,10 @@
+package javaCore.oop_2;
+
+public class Bicycle {
+    public String modelName;
+    public int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}
